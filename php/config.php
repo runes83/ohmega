eb680a51190426a80b91a6235266f2966db2d4ef
Rune Synnevåg <runes@unipluss.no>
2015-12-31 11:51:47 +0100

    init commit

---
+++ php/config.php
@@ -0,0 +1,9 @@
+<?php
+
+/* ---------------------------------------------------------------------- */
+/*	Contact Form
+/* ---------------------------------------------------------------------- */
+
+// The email address where all contact form messages are sent (inbox)
+$your_email_address = 'replaceme@withyourmailaddress.com';
+
